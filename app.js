9f889be32c99e51e8e10b8684194bd3b94185d24
GrobelnyHubert <grobelny.hubert@gmail.com>
2022-02-19 12:18:43 +0100

    add heroku changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const exp = require('constants');
 const express = require('express');
 const path = require("path");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const functions = require("./functions");
 
 app.set("view engine", "hbs");
