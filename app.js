0275a63808efb3145be61d05f0e49b595d221da6
GrobelnyHubert <grobelny.hubert@gmail.com>
2022-02-19 11:58:48 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const exp = require('constants');
+const express = require('express');
+const path = require("path");
+const app = express();
+const port = 3000;
+const functions = require("./functions");
+
+app.set("view engine", "hbs");
+app.use("/assets", express.static(path.join(__dirname, "./assets")));
+app.use("/js", express.static(path.join(__dirname, "./js")));
+
+app.get("/", function (req, res){
+    res.render("index", {
+        pageTitle: "Lekcja ALK 19.02",
+        y: functions.add(2 ,6)
+    });
+})
+
+app.get("/about", function (req, res){
+    res.render("about")
+})
+
+app.listen(port, () => {
+    console.log(`Serwer dziala na porcie ${port}`);
+})
---
+++ js/func.js
@@ -0,0 +1,5 @@
+// alert('hello');
+
+document.getElementById("btn").addEventListener("click", function(){
+    document.getElementById("show").innerText = document.getElementById("val").value;
+})
